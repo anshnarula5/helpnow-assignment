1901b8efa2685357efb4b0d3a5eaf59a8d7a7f20
Ansh Narula <2019210@iiitdmj.ac.in>
2022-01-04 01:05:05 +0530

    added redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import LineBar from "./components/LineBar";
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(
+        "http://api.openweathermap.org/data/2.5/forecast?q=bhopal&appid=73dee316d83fc8e4312e68d912a37b42"
+      );
+      setData(res.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      HI
+      {data && <LineBar data={data} />}
     </div>
   );
 }
